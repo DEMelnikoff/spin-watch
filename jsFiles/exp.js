dd1717e6e250011b7ee81a0c5acdd07f8db4a36b
David E Melnikoff <davidemelnikoff@gmail.com>
2025-07-13 20:15:15 +0200

    Update exp.js

--- jsFiles/exp.js
+++ jsFiles/exp.js
@@ -218,7 +218,7 @@ const exp = (function() {
             {
                 prompt: `How <b>happy</b> are you right now?`, 
                 name: `happiness`, 
-                options: ['0 = Very Unhappy', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10 = Very Happy'],
+                options: ['10 = Very Unhappy', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0 = Very Happy'],
             },
         ],
         scale_width: 500,
