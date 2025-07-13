0a954c4ac08e262821c914f2be99bdecd846144d
David E Melnikoff <davidemelnikoff@gmail.com>
2025-07-13 13:14:38 +0200

    Update utils.js

--- jsFiles/utils.js
+++ jsFiles/utils.js
@@ -25,7 +25,7 @@ jsPsych.data.addProperties({ subject: subject_id });
 const filename = `${subject_id}.csv`;
 
 // define completion code for Prolific
-const completionCode = "CQOX2PZL";
+const completionCode = "CW0CMZ8Y";
 
 // when true, boot participant from study without redirecting to Prolific
 let boot = false;
